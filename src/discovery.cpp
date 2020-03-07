24dd318cb552b9344d40b72983c6a93a08caf74c
Enrico Gueli <enrico.gueli@gmail.com>
2020-03-07 17:01:19 +0100

    implement search algo

--- src/discovery.cpp
+++ src/discovery.cpp
@@ -3,6 +3,8 @@
 
 #include <Arduino.h>
 #include <mdns.h>
+#include <map>
+#include <set>
 
 // TODO convert to constants
 #define QUESTION_SERVICE "_spotify-connect._tcp.local"
@@ -15,6 +17,15 @@
 #define HOSTS_ADDRESS 3
 String hosts[MAX_HOSTS][4]; // Array containing information about hosts received over mDNS.
 
+bool querySent;
+
+const size_t kMaxEntries = 4;
+std::set<String> services;
+std::map<String, String> serviceToHost;
+std::map<String, String> hostToAddress;
+
+String ipAddress;
+
 // When an mDNS packet gets parsed this callback gets called once per Query.
 // See mdns.h for definition of mdns::Query.
 void answerCallback(const mdns::Answer *answer)
@@ -25,32 +36,13 @@ void answerCallback(const mdns::Answer *answer)
     //  name:    Mosquitto MQTT server on twinkle.local
     if (answer->rrtype == MDNS_TYPE_PTR and strstr(answer->name_buffer, QUESTION_SERVICE) != 0)
     {
-        Serial.printf("got PTR response\n");
-        unsigned int i = 0;
-        for (; i < MAX_HOSTS; ++i)
+        if (services.size() >= kMaxEntries)
         {
-            if (hosts[i][HOSTS_SERVICE_NAME] == answer->rdata_buffer)
-            {
-                // Already in hosts[][].
-                break;
-            }
-            if (hosts[i][HOSTS_SERVICE_NAME] == "")
-            {
-                // This hosts[][] entry is still empty.
-                hosts[i][HOSTS_SERVICE_NAME] = answer->rdata_buffer;
-                break;
-            }
+            Serial.println("got too many services, ignoring");
         }
-        if (i == MAX_HOSTS)
+        else
         {
-            Serial.print(" ** ERROR ** No space in buffer for ");
-            Serial.print('"');
-            Serial.print(answer->name_buffer);
-            Serial.print('"');
-            Serial.print("  :  ");
-            Serial.print('"');
-            Serial.println(answer->rdata_buffer);
-            Serial.print('"');
+            services.insert(String(answer->rdata_buffer));
         }
     }
 
@@ -60,87 +52,53 @@ void answerCallback(const mdns::Answer *answer)
     //  data:    p=0;w=0;port=1883;host=twinkle.local
     if (answer->rrtype == MDNS_TYPE_SRV)
     {
-        Serial.printf("got SRV response\n");
-        unsigned int i = 0;
-        for (; i < MAX_HOSTS; ++i)
+        if (serviceToHost.size() >= kMaxEntries)
         {
-            if (hosts[i][HOSTS_SERVICE_NAME] == answer->name_buffer)
+            Serial.println("got too many interesting SRV responses, ignoring");
+        }
+        else
+        {
+            char *port_start = strstr(answer->rdata_buffer, "port=");
+            if (port_start)
             {
-                // This hosts entry matches the name of the host we are looking for
-                // so parse data for port and hostname.
-                char *port_start = strstr(answer->rdata_buffer, "port=");
-                if (port_start)
-                {
-                    port_start += 5;
-                    char *port_end = strchr(port_start, ';');
-                    char port[1 + port_end - port_start];
-                    strncpy(port, port_start, port_end - port_start);
-                    port[port_end - port_start] = '\0';
+                port_start += 5;
+                char *port_end = strchr(port_start, ';');
+                char port[1 + port_end - port_start];
+                strncpy(port, port_start, port_end - port_start);
+                port[port_end - port_start] = '\0';
 
-                    if (port_end)
+                if (port_end)
+                {
+                    char *host_start = strstr(port_end, "host=");
+                    if (host_start)
                     {
-                        char *host_start = strstr(port_end, "host=");
-                        if (host_start)
-                        {
-                            host_start += 5;
-                            hosts[i][HOSTS_PORT] = port;
-                            hosts[i][HOSTS_HOST_NAME] = host_start;
-                        }
+                        host_start += 5;
+                        serviceToHost.insert({String(answer->name_buffer), String(host_start)});
+                    }
+                    else
+                    {
+                        Serial.println("malformed answer (no 'host=' found)");
                     }
                 }
-                break;
+                else
+                {
+                    Serial.println("malformed answer (no port delimiter found)");
+                }
             }
-        }
-        if (i == MAX_HOSTS)
-        {
-            Serial.print(" Did not find ");
-            Serial.print('"');
-            Serial.print(answer->name_buffer);
-            Serial.print('"');
-            Serial.println(" in hosts buffer.");
+            else
+            {
+                Serial.println("malformed answer (no 'port=' found)");
+            }
         }
     }
 
-    // A typical A record matches an FQDN to network ipv4 address.
-    // eg:
-    //   name:    twinkle.local
-    //   address: 192.168.192.9
     if (answer->rrtype == MDNS_TYPE_A)
     {
-        Serial.printf("got A response\n");
-        int i = 0;
-        for (; i < MAX_HOSTS; ++i)
+        if (hostToAddress.size() >= kMaxEntries)
         {
-            if (hosts[i][HOSTS_HOST_NAME] == answer->name_buffer)
-            {
-                hosts[i][HOSTS_ADDRESS] = answer->rdata_buffer;
-                break;
-            }
+            Serial.println("got too many interesting A responses, ignoring");
         }
-        if (i == MAX_HOSTS)
-        {
-            Serial.print(" Did not find ");
-            Serial.print('"');
-            Serial.print(answer->name_buffer);
-            Serial.print('"');
-            Serial.println(" in hosts buffer.");
-        }
-    }
-
-    Serial.println();
-    for (int i = 0; i < MAX_HOSTS; ++i)
-    {
-        if (hosts[i][HOSTS_SERVICE_NAME] != "")
-        {
-            Serial.print(">  ");
-            Serial.print(hosts[i][HOSTS_SERVICE_NAME]);
-            Serial.print("    ");
-            Serial.print(hosts[i][HOSTS_PORT]);
-            Serial.print("    ");
-            Serial.print(hosts[i][HOSTS_HOST_NAME]);
-            Serial.print("    ");
-            Serial.println(hosts[i][HOSTS_ADDRESS]);
-        }
+        hostToAddress.insert({String(answer->name_buffer), String(answer->rdata_buffer)});
     }
 }
 
@@ -151,14 +109,12 @@ void setupDiscovery()
 {
 }
 
-bool querySent;
-
-void sendQuery() {
-    // Query for all host information for a paticular service. ("_mqtt" in this case.)
+void sendQuery(unsigned int type, const char *queryName)
+{
     my_mdns.Clear();
     struct mdns::Query query;
-    strncpy(query.qname_buffer, QUESTION_SERVICE, MAX_MDNS_NAME_LEN);
-    query.qtype = MDNS_TYPE_PTR;
+    strncpy(query.qname_buffer, queryName, MAX_MDNS_NAME_LEN);
+    query.qtype = type;
     query.qclass = 1; // "INternet"
     query.unicast_response = 0;
     my_mdns.AddQuery(query);
@@ -167,13 +123,36 @@ void sendQuery() {
 
 void loopDiscovery()
 {
-    if (!isWifiConnected()) {
+    // Did we do our job already?
+    if (!ipAddress.isEmpty())
+    {
         return;
     }
-    if (!querySent) {
-        Serial.println("sending MDNS query");
-        sendQuery();
+    // Can we actually search?
+    if (!isWifiConnected())
+    {
+        return;
+    }
+    if (!querySent)
+    {
+        Serial.println("sending MDNS PTR query");
+        sendQuery(MDNS_TYPE_PTR, QUESTION_SERVICE);
         querySent = true;
     }
     my_mdns.loop();
+
+    for (auto service : services)
+    {
+        auto hostIt = serviceToHost.find(service);
+        if (hostIt != serviceToHost.end())
+        {
+            String host = hostIt->second;
+            auto addrIt = hostToAddress.find(host);
+            if (addrIt != hostToAddress.end())
+            {
+                ipAddress = addrIt->second;
+                Serial.printf("FOUND service '%s' at host '%s' with address '%s'\n", service.c_str(), host.c_str(), ipAddress.c_str());
+            }
+        }
+    }
 }
