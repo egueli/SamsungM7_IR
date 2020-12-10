5c7886815a385b296120ee35b1bdd91f26769188
Enrico Gueli <enrico.gueli@gmail.com>
2020-12-10 22:04:47 +0100

    extract URL generation; reformat

--- src/speaker_musiccast.cpp
+++ src/speaker_musiccast.cpp
@@ -1,17 +1,18 @@
 #include "speaker_musiccast.h"
 #include "http_response.h"
 
-void MusicCastSpeaker::setAddress(const String &address) {
+void MusicCastSpeaker::setAddress(const String &address)
+{
     ipAddress = address;
 }
 
-bool MusicCastSpeaker::isAddressValid() {
-    if (ipAddress.isEmpty()) {
+bool MusicCastSpeaker::isAddressValid()
+{
+    String url;
+    if (!getBaseUrl(url, "getStatus"))
+    {
         return false;
     }
-
-    String url;
-    url = "http://" + ipAddress + "/YamahaExtendedControl/v1/main/getStatus";
 
     request.open("GET", url.c_str());
     request.send();
@@ -19,18 +20,37 @@ bool MusicCastSpeaker::isAddressValid() {
     return waitForHttpOkResponse(request);
 }
 
-int MusicCastSpeaker::getVolume() {
+int MusicCastSpeaker::getVolume()
+{
     return 0;
 }
 
-bool MusicCastSpeaker::setVolume(int newVolume) {
+bool MusicCastSpeaker::setVolume(int newVolume)
+{
     return true;
 }
 
-bool MusicCastSpeaker::setTvInput() {
+bool MusicCastSpeaker::setTvInput()
+{
     return true;
 }
 
-bool MusicCastSpeaker::toggleMute() {
+bool MusicCastSpeaker::toggleMute()
+{
+    return true;
+}
+
+/**
+ * Puts the base URL to send requests to the speaker
+ * \return true if the IP address is known and a URL can be constructed, false otherwise.
+ */
+bool MusicCastSpeaker::getBaseUrl(String &output, const String &endPart)
+{
+    if (ipAddress.isEmpty())
+    {
+        return false;
+    }
+
+    output = "http://" + ipAddress + "/YamahaExtendedControl/v1/main/" + endPart;
     return true;
 }
--- src/speaker_musiccast.h
+++ src/speaker_musiccast.h
@@ -21,5 +21,6 @@ public:
 private:
     String ipAddress;
     asyncHTTPrequest request;
+    bool getBaseUrl(String &output, const String &endPart);
 };
 
