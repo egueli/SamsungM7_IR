09e84b3fd950b36962bb23a707a3dab2dbea2b20
Enrico Gueli <enrico.gueli@gmail.com>
2022-08-24 23:10:40 +0200

    blink

--- src/user_main.c
+++ src/user_main.c
@@ -1,4 +1,21 @@
+#include "esp_common.h"
+#include "freertos/task.h"
+#include "gpio.h"
 
-void user_init() {
+void task_blink(void* ignore)
+{
+    GPIO_OUTPUT_SET(2, 1);
+    while(true) {
+    	GPIO_OUTPUT_SET(2, 1);
+        vTaskDelay(1000/portTICK_RATE_MS);
+    	GPIO_OUTPUT_SET(2, 0);
+        vTaskDelay(1000/portTICK_RATE_MS);
+    }
+
+    vTaskDelete(NULL);
 }
 
+void user_init(void)
+{
+    xTaskCreate(&task_blink, "startup", 2048, NULL, 1, NULL);
+}
